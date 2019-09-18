94ded23052298329ee9ffb9241ad8ffc66a58bdd
Krystof Rehacek <krystofee@gmail.com>
2019-09-18 23:25:17 +0200

    initial commit

---
+++ enhance-merge-requests.js
@@ -0,0 +1,38 @@
+if (
+  /^https:\/\/gitlab.com\/\w+\/\w+\/merge_requests$/.test(window.location.href)
+) {
+  function createThreadsBadge(element, color, resolved, resolvable) {
+    const li = $("<li/>")
+      .addClass("issuable-comments d-none d-sm-flex")
+      .prependTo(element);
+    $("<span/>")
+      .addClass("badge color-label")
+      .css("background-color", color)
+      .css("color", "#333333")
+      .text(`${resolved}/${resolvable} threads resolved`)
+      .prependTo(li);
+  }
+
+  $(".merge-request").each(function() {
+    const anchor = $(this).find(".merge-request-title-text a")[0];
+    const metaList = $(this).find(".issuable-meta ul")[0];
+
+    $.ajax({
+      url: `${anchor.href}/discussions.json`,
+      success: function(result) {
+        let resolvable = 0;
+        let resolved = 0;
+        result.forEach(item => {
+          if (item.resolvable) resolvable++;
+          if (item.resolved) resolved++;
+        });
+
+        if (resolvable > resolved) {
+          createThreadsBadge(metaList, "#ffd3d3", resolved, resolvable);
+        } else if (resolved === resolvable && resolvable > 0) {
+          createThreadsBadge(metaList, "#8fc7a6", resolved, resolvable);
+        }
+      }
+    });
+  });
+}
