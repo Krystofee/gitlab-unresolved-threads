e7b4e61e320bf12305041a3db0982ee17f2431da
Jan Kaifer <kaifer741@gmail.com>
2019-10-16 18:06:46 +0200

    added timers to handle non refreshing url changes

--- ignore-whitespaces.js
+++ ignore-whitespaces.js
@@ -1,15 +1,28 @@
-if (
-  /^https:\/\/gitlab\.com.*\/diff/.test(window.location.href) &&
-  !/\?.*w=1/.test(window.location.href)
-) {
-  var str = "";
-  if (/\?(.+=.*&)*$/.test(window.location.href)) {
-    str = "";
-  } else if (/\?(.+=.*&)*.+=.*[^&]$/.test(window.location.href)) {
-    str = "&";
-  } else {
-    str = "?";
-  }
+var isRefreshing = false;
 
-  window.location = window.location.toString() + str + "w=1";
+function doIt() {
+  if (
+    !isRefreshing &&
+    /^https:\/\/gitlab\.com.*\/diffs/.test(window.location.href) &&
+    !/\?.*w=1/.test(window.location.href)
+  ) {
+    var str = "";
+    if (/\?(.+=.*&)*$/.test(window.location.href)) {
+      str = "";
+    } else if (/\?(.+=.*&)*.+=.*[^&]$/.test(window.location.href)) {
+      str = "&";
+    } else {
+      str = "?";
+    }
+
+    window.location = window.location.toString() + str + "w=1";
+    isRefreshing = true;
+    setTimeout(function() {
+      isRefreshing = false;
+    }, 2000);
+  }
 }
+
+doIt();
+
+setInterval(doIt, 200);
